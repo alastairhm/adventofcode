57b9e9f2d0c686b5eacafa6df9da0fa6e2340985
Alastair Montgomery <alastair.montgomery@bjss.com>
2022-12-01 09:32:05 +0000

    day1

---
+++ day1/counter.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+elf = np.array()
+
+f = open("input.txt","r")
+lines = f.readlines()
+
+total = 0
+for line in lines:
+    if line == "\n":
+        elf = np.append(elf, total)
+        total = 0
+    else:
+        total += int(line.replace("\n",""))
+
+print(elf)
+print(np.max(elf))
+nps = np.sort(elf)[::-1]
+print (nps[0]+nps[1]+nps[2])
