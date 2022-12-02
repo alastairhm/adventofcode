7e3f095b5323b58a39ba64455ec9d5145905a329
Alastair Montgomery <alastair.montgomery@bjss.com>
2022-12-02 14:40:31 +0000

    Day2 refactor

--- day2/score.py
+++ day2/score.py
@@ -15,21 +15,21 @@ score=0
 for round in elf:
     if round == "RP":
         score += 2 + 6
-    if round == "PP":
+    elif round == "PP":
         score += 2 + 3
-    if round == "SP":
+    elif round == "SP":
         score += 2
-    if round == "RR":
+    elif round == "RR":
         score += 1 + 3
-    if round == "PR":
+    elif round == "PR":
         score += 1
-    if round == "SR":
+    elif round == "SR":
         score += 1 + 6
-    if round == "RS":
+    elif round == "RS":
         score += 3 
-    if round == "PS":
+    elif round == "PS":
         score += 3 + 6
-    if round == "SS":
+    elif round == "SS":
         score += 3 + 3
 
 print(score)
--- day2/score2.py
+++ day2/score2.py
@@ -13,23 +13,23 @@ for round in elf:
     if round[1] == "X": #Lose
         if round[0] == "R":
             fix = round[0] + "S"
-        if round[0] == "P":
+        elif round[0] == "P":
             fix = round[0] + "R"
-        if round[0] == "S":
+        elif round[0] == "S":
             fix = round[0] + "P"
-    if round[1] == "Y": #Draw
+    elif round[1] == "Y": #Draw
         if round[0] == "R":
             fix = round[0] + "R"
-        if round[0] == "P":
+        elif round[0] == "P":
             fix = round[0] + "P"
-        if round[0] == "S":
+        elif round[0] == "S":
             fix = round[0] + "S"
-    if round[1] == "Z": #Win
+    elif round[1] == "Z": #Win
         if round[0] == "R":
             fix = round[0] + "P"
-        if round[0] == "P":
+        elif round[0] == "P":
             fix = round[0] + "S"
-        if round[0] == "S":
+        elif round[0] == "S":
             fix = round[0] + "R"
     fixed.append(fix)
 
@@ -37,21 +37,21 @@ score=0
 for round in fixed:
     if round == "RP":
         score += 2 + 6
-    if round == "PP":
+    elif round == "PP":
         score += 2 + 3
-    if round == "SP":
+    elif round == "SP":
         score += 2
-    if round == "RR":
+    elif round == "RR":
         score += 1 + 3
-    if round == "PR":
+    elif round == "PR":
         score += 1
-    if round == "SR":
+    elif round == "SR":
         score += 1 + 6
-    if round == "RS":
+    elif round == "RS":
         score += 3 
-    if round == "PS":
+    elif round == "PS":
         score += 3 + 6
-    if round == "SS":
+    elif round == "SS":
         score += 3 + 3
 
 print(score)
