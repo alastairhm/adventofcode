960acb19ff0d86ab2198f7a49a5d9783b8e3c024
Alastair Montgomery <alastair@montgomery.me.uk>
2023-11-15 17:19:49 +0000

    Starting 2015 challenge for funs

---
+++ 2015/day1/index.js
@@ -0,0 +1,37 @@
+console.log("2015 Day 1");
+
+const tools = require("../../tools/tools");
+
+floor = 0;
+array = tools.readData(__dirname+"/input.txt");
+
+for (const line of array) {
+    for (const char of line){
+        if (char == "(") {
+            floor += 1;
+        } else {
+            floor -= 1;
+        }
+    }
+    console.log("Delivery floor = ", floor)
+}
+
+floor = 0;
+position = 1;
+basement = 0;
+for (const line of array) {
+    for (const char of line){
+        if (char == "(") {
+            floor += 1;
+        } else {
+            floor -= 1;
+        }
+        if (floor < 0){
+            basement = position;
+            break;
+        } else {
+            position++;
+        }
+    }
+    console.log("Position = ", position)
+}
