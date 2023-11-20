8d5ed233ee60f53098709568849c6a65e1480bf8
Alastair Montgomery <alastair@montgomery.me.uk>
2023-11-20 16:11:59 +0000

    2015 Day 5 part 2 regex fun

---
+++ 2015/day5/index_part2.js
@@ -0,0 +1,31 @@
+console.log("2015 Day 5 Part 2");
+
+import path from "path";
+import { fileURLToPath } from "url";
+import * as tools from "../../tools/tools.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function process(array){
+  let nice = 0;
+  let naughty = 0;
+    for (const line of array){
+      const pairs = line.search(/(..).*\1/) >= 0;
+      const reps = line.search(/(.).\1/) >= 0;
+      if (pairs && reps){
+        nice +=1;
+      } else {
+        naughty +=1;
+      }
+    }
+  console.log('Nice =',nice);
+  console.log('Naughty =',naughty);
+}
+
+const filename = ["input.txt"];
+
+for ( const current of filename){
+    const array = tools.readData(path.join(__dirname,current));
+    process(array);
+}
