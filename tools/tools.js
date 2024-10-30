02aadca0cd64c22db73b068cc1df0ecad27c17e3
Alastair Montgomery <alastair@montgomery.me.uk>
2024-10-30 14:09:42 +0000

    2024 placeholder

--- tools/tools.js
+++ tools/tools.js
@@ -1,8 +1,14 @@
 module.exports = {
-  readData: function (filename) {
-    console.log("Reading data :", filename);
-    const fs = require("fs");
-    const array = fs.readFileSync(filename, "utf-8").toString().split("\n");
-    return array;
-  },
+readData: function (filename) {
+  console.log("Reading data:", filename);
+  const fs = require("fs");
+
+  try {
+    const data = fs.readFileSync(filename, "utf-8").toString().split("\n");
+    return data;
+  } catch (err) {
+    console.error("Error reading file:", err);
+    throw err;
+  }
+}  
 };
